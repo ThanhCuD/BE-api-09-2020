479b53a0931959fb146778b86819ba86ff491914
ThanhCuD <thanh.duc@websparks.sg>
2020-10-05 17:48:15 +0700

    add new

---
+++ Solution1/Plife.Data/Models/EmployeeContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PLife.Data.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Uncle",
+                LastName = "Bob",
+                Email = "uncle.bob@gmail.com",
+                DateOfBirth = new DateTime(1979, 04, 25),
+                PhoneNumber = "999-888-7777"
+            }, new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Jan",
+                LastName = "Kirsten",
+                Email = "jan.kirsten@gmail.com",
+                DateOfBirth = new DateTime(1981, 07, 13),
+                PhoneNumber = "111-222-3333"
+            });
+        }
+    }
+}
---
+++ Solution1/Plife.Data/Models/Repository/IDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PLife.Data.Models.Repository
+{
+    public interface IDataRepository<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression);
+        TEntity Get(long id);
+        void Add(TEntity entity);
+        void Update(TEntity dbEntity, TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
